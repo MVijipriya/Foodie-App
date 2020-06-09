f592acce347c138f838d1c9616a43997f5df4675
MVijipriya <42040953+MVijipriya@users.noreply.github.com>
2020-06-09 22:28:25 +0530

    Add files via upload

---
+++ build/precache-manifest.1a4a17caf68ab15798bb1d9c782233f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0d43802023febbdaa940e593eef0a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae4f25b214d6faef1b97",
+    "url": "/static/css/main.b3d2c1a5.chunk.css"
+  },
+  {
+    "revision": "ee8b009d31ec8a9b1368",
+    "url": "/static/js/2.ae07161a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ae07161a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae4f25b214d6faef1b97",
+    "url": "/static/js/main.586e8dc3.chunk.js"
+  },
+  {
+    "revision": "e272b59e3fd41c732d7d",
+    "url": "/static/js/runtime-main.8735f059.js"
+  },
+  {
+    "revision": "eef9360f67e9d17d1471bb6bea74d98b",
+    "url": "/static/media/foods.eef9360f.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
